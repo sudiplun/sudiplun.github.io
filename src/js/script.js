84595452bcb30872cc38edee6ec84372b082725d
sudiplun <sudeeplun@gmail.com>
2025-06-17 10:03:14 +0545

    wipe/new design

--- src/js/script.js
+++
@@ -1,7 +0,0 @@
-const menuToggle = document.getElementById("menu-toggle");
-const navLinks = document.getElementById("nav-links");
-
-menuToggle.addEventListener("click", () => {
-  navLinks.classList.toggle("hidden"); // Show/hide the menu
-  navLinks.classList.toggle("grid"); // Ensure flex layout for links
-});
